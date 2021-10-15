fc4ecfebc0474d81f1d927347d3bbd1c22826952
lcampbell2 <lcampbell@MauronHaxmaster.localdomain>
2021-10-15 13:10:38 -0300

    create group posts on client-side

--- client/src/pages/groups/[slug].tsx
+++ client/src/pages/groups/[slug].tsx
@@ -1,12 +1,32 @@
-import { Box, Divider, Flex, Heading, Stack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Divider,
+  FormControl,
+  FormLabel,
+  Heading,
+  Select,
+  Stack,
+  Text,
+  Textarea,
+  useToast,
+} from "@chakra-ui/react";
+import { Formik, Form } from "formik";
 import { NextPage } from "next";
 import React from "react";
+import { InputField } from "../../components/InputField";
 import { TextPost } from "../../components/post/TextPost";
 import { UserCard } from "../../components/user/UserCard";
-import { useGroupBySlugQuery } from "../../generated/graphql";
+import {
+  useGroupBySlugQuery,
+  useCreatePostMutation,
+} from "../../generated/graphql";
+import { toErrorMap } from "../../utils/toErrorMap";
 
 export const GroupDetails: NextPage<{ slug: string }> = ({ slug }) => {
-  const [{ data, fetching, error }, _] = useGroupBySlugQuery({
+  const toast = useToast();
+  const [_, createPost] = useCreatePostMutation();
+  const [{ data, fetching, error }, _query] = useGroupBySlugQuery({
     variables: { slug: slug },
   });
 
@@ -40,10 +60,10 @@ export const GroupDetails: NextPage<{ slug: string }> = ({ slug }) => {
   } else {
     userList = data?.groupBySlug?.users.map(({ user, role }, idx) => {
       return (
-        <>
-          <UserCard key={idx} displayName={user.displayName} role={role} />
+        <Box key={idx}>
+          <UserCard displayName={user.displayName} role={role} />
           <Divider borderBottomColor='gray.900' />
-        </>
+        </Box>
       );
     });
   }
@@ -58,16 +78,15 @@ export const GroupDetails: NextPage<{ slug: string }> = ({ slug }) => {
   } else {
     groupPosts = data?.groupBySlug?.posts?.map((post) => {
       return (
-        <>
+        <Box key={post.id}>
           <TextPost
-            key={post.id}
             title={post.title}
             description={post.description}
             date={post.updatedAt}
             authorName={post.author.displayName}
           />
           <Divider borderBottomColor='gray.900' />
-        </>
+        </Box>
       );
     });
   }
@@ -79,6 +98,43 @@ export const GroupDetails: NextPage<{ slug: string }> = ({ slug }) => {
       {groupInfo}
       <Text>Users:</Text>
       {userList}
+      <Box>
+        <Text fontSize='lg' fontWeight='bold'>
+          New Post:
+        </Text>
+        <Formik
+          initialValues={{
+            groupId: data?.groupBySlug?.id as number,
+            title: "",
+            description: "",
+          }}
+          onSubmit={async (values, { setErrors }) => {
+            const res = await createPost(values);
+            if (res?.data?.createPost.errors) {
+              setErrors(toErrorMap(res.data.createPost.errors));
+            } else if (res.data?.createPost.post) {
+              toast({
+                title: "Post successfully created.",
+                description: `createPost success`,
+                status: "success",
+                duration: 9000,
+                isClosable: true,
+              });
+            }
+          }}
+        >
+          {({ handleChange }) => (
+            <Form>
+              <InputField name='title' label='Title' onChange={handleChange} />
+              <FormControl>
+                <FormLabel htmlFor='description'>Description</FormLabel>
+                <Textarea name='description' onChange={handleChange} />
+              </FormControl>
+              <Button type='submit'>Create Post</Button>
+            </Form>
+          )}
+        </Formik>
+      </Box>
       <Text>Posts:</Text>
       {groupPosts}
     </Box>
